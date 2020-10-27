1500fa2ad722bfcac42c1bc3f475f384951abbe6
VBF18 <toddyabc@gmail.com>
2020-10-27 20:53:18 -0300

    Criamos a classe Usuario

--- Sql.php
+++ Sql.php
@@ -6,30 +6,30 @@ class Sql extends PDO
 
 	public function __construct()
 	{
-		$this->conn = new PDO("mysql:dbname=dbcursophp;host=localhost", "root", "jwFBR6KemHP%");
+		$this->conn = new PDO("mysql:dbname=dbcursophp;host=localhost", "root", "jwFBR6KemHP%");		
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);		
 		}
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $Value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $Value);
 	}
 
 	public function query($rawQuery, $params = array()){
-		$stmt = $this->conn->prepare($rawQuery);
-		$this->setParams($stmt, $params);
+		$stmt = $this->conn->prepare($rawQuery);		
+		$this->setParams($stmt, $params);		
 		$stmt->execute();
-		return $stmt;
+		return $stmt;		
 	}
 
 	public function select($rawQuery, $params = array()):array //declaração de retorno de função/método, que tem o objetivo de declarar que, esta dada função/método retorna um array. 
 	{
 		$stmt = $this->query($rawQuery, $params);
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);		
 	}
 }
 
