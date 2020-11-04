8f6f75c8b7f35979aee76ddac85121392c506c9d
VBF18 <toddyabc@gmail.com>
2020-11-04 18:55:44 -0300

    Adicionado várias funções de pesquisa

--- index.php
+++ index.php
@@ -6,8 +6,26 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+//*** Carrega um Usuário ***//
+/*$root = new Usuario();
 $root->loadById(5);
-echo $root;
+echo $root;*/
+
+//*** Carrega uma Lista de  Usuários ***//
+/*$lista = Usuario::getList();
+echo "<pre>";
+print_r($lista); 
+echo "</pre>";*/
+
+//*** Carrega uma Lista de  Usuários Buscando pelo Login ***//
+/*$search = Usuario::search("m");
+echo "<pre>";
+print_r($search); 
+echo "</pre>"*/
+
+//*** Carrega um Usuário Usando o Login e a Senha ***//
+$usuario = new Usuario();
+$usuario->login("tedi", "hhsdy78");
+echo $usuario; 
 
 ?>
